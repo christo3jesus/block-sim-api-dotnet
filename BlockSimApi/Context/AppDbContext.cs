d73a3deeb827d3f6c81c9fab3980af103bd231f0
Christopher Jesus <christopher3jesus@gmail.com>
2024-10-26 12:02:13 -0500

    feat: add db context

---
+++ BlockSimApi/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlockSimApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlockSimApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Block> Blocks { get; set; }
+    }
+}
