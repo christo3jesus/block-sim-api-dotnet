d5d38b241dc6056b25331dfe34cb64c7df1eda23
Christopher Jesus <christopher3jesus@gmail.com>
2024-10-26 01:29:03 -0500

    feat: add block model

---
+++ BlockSimApi/Models/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlockSimApi.Models
+{
+    public class Block
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; private set; } = DateTime.UtcNow;
+
+        [MaxLength(64)]
+        public string Hash { get; private set; } = null!;
+
+        [MaxLength(64)]
+        public string PreviousHash { get; private set; } = null!;
+
+        public virtual ICollection<Transaction> Transactions { get; private set; } = new List<Transaction>();
+    }
+}
