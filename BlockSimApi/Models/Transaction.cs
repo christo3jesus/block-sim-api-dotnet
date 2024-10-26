5655956cc7e7a24e08edf5f9b09ef879e63c1a19
Christopher Jesus <christopher3jesus@gmail.com>
2024-10-26 01:27:01 -0500

    feat: add transaction model

---
+++ BlockSimApi/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlockSimApi.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [MaxLength(42)]
+        public string Sender { get; set; } = null!;
+
+        [MaxLength(42)]
+        public string Receiver { get; set; } = null!;
+
+        [Precision(18, 4)]
+        public decimal Amount { get; set; }
+
+        public DateTime Timestamp { get; private set; } = DateTime.UtcNow;
+    }
+}
