1a950450f19bdc15496fae6010446e6dd784a26b
Christopher Carrasco <christopher3jesus@gmail.com>
2025-03-02 19:49:47 -0500

    fix: update blocks controller

--- BlockSimApi/Controllers/BlocksController.cs
+++ BlockSimApi/Controllers/BlocksController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BlockSimApi.Context;
 using BlockSimApi.Models;
+using BlockSimApi.Dtos;
 
 namespace BlockSimApi.Controllers
 {
@@ -23,38 +19,77 @@ namespace BlockSimApi.Controllers
 
         // GET: api/Blocks
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Block>>> GetBlocks()
+        public async Task<ActionResult<IEnumerable<BlockDto>>> GetBlocks()
         {
-            return await _context.Blocks.ToListAsync();
+            var blocks = await _context.Blocks
+                .OrderByDescending(b => b.Id)
+                .Select(b => new BlockDto
+                {
+                    Id = b.Id,
+                    Timestamp = b.Timestamp,
+                    Sentence = b.Sentence,
+                    Hash = b.Hash,
+                    PreviousHash = b.PreviousHash,
+                    TransactionCount = _context.Transactions.Count(t => t.BlockId == b.Id)
+                })
+                .ToListAsync();
+
+            return Ok(blocks);
         }
 
         // GET: api/Blocks/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Block>> GetBlock(int id)
+        public async Task<ActionResult<BlockDto>> GetBlock(int id)
         {
-            var block = await _context.Blocks.FindAsync(id);
+            var block = await _context.Blocks
+                .Where(b => b.Id == id)
+                .Select(b => new BlockDto
+                {
+                    Id = b.Id,
+                    Timestamp = b.Timestamp,
+                    Sentence = b.Sentence,
+                    Hash = b.Hash,
+                    PreviousHash = b.PreviousHash,
+                    TransactionCount = _context.Transactions.Count(t => t.BlockId == b.Id)
+                })
+                .FirstOrDefaultAsync();
 
             if (block == null)
             {
                 return NotFound();
             }
 
-            return block;
+            return Ok(block);
         }
 
         // POST: api/Blocks
         [HttpPost]
-        public async Task<ActionResult<Block>> PostBlock(Block block)
+        public async Task<ActionResult<BlockDto>> CreateBlock(CreateBlockDto createBlockDto)
         {
-            _context.Blocks.Add(block);
+            if (string.IsNullOrWhiteSpace(createBlockDto.Sentence))
+            {
+                return BadRequest("Empty is not allowed.");
+            }
+
+            var lastBlock = await _context.Blocks.FirstOrDefaultAsync();
+
+            var newBlock = new Block(
+                createBlockDto.Sentence,
+                lastBlock?.Hash ?? "0".PadLeft(64, '0')
+            );
+
+            _context.Blocks.Add(newBlock);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetBlock), new { id = block.Id }, block);
-        }
-
-        private bool BlockExists(int id)
-        {
-            return _context.Blocks.Any(e => e.Id == id);
+            return CreatedAtAction(nameof(GetBlock), new { id = newBlock.Id }, new BlockDto
+            {
+                Id = newBlock.Id,
+                Timestamp = newBlock.Timestamp,
+                Sentence = newBlock.Sentence,
+                Hash = newBlock.Hash,
+                PreviousHash = newBlock.PreviousHash,
+                TransactionCount = 0
+            });
         }
     }
 }
